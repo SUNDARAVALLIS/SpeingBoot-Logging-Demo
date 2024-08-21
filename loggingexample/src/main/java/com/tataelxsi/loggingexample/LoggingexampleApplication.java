0ca66fbe69dc5031537bedc9d1551608da4a9aa5
SUNDARAVALLIS <113921048+SUNDARAVALLIS@users.noreply.github.com>
2024-08-21 09:13:18 +0530

    Add files via upload

---
+++ loggingexample/src/main/java/com/tataelxsi/loggingexample/LoggingexampleApplication.java
@@ -0,0 +1,13 @@
+package com.tataelxsi.loggingexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingexampleApplication.class, args);
+	}
+
+}
